3a5651d3ba7e25d1c91021b6dbd092d9011a7090
zelda <shwarzbergzelda@gmail.com>
2026-06-08 16:57:21 -0400

    design main page

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+import { posts } from './data.js'
+const postsSection = document.getElementById('posts-section')
 
+const postsHtml = posts.map(individualPost => {
+    const { name, username, location, avatar, post, comment, likes } = individualPost
+
+    return `
+        <article class="post">
+            <div class="post-header">
+                <img class="post-avatar" src="${avatar}" alt="user avatar">
+                
+                <div class="user-info">
+                    <h2 class="user-name">${name}</h2>
+                    <p class="user-location">${location}</p>
+                </div>
+            </div>
+
+            <div class="main-post">
+                <img src="${post}" alt="post image">
+            </div>
+
+            <div class="post-footer">
+                <div class="interactions">
+                    <img id="like-btn" src="./images/icon-heart.png" alt="like button">
+                    <img id="comment-btn" src="./images/icon-comment.png" alt="comment button">
+                    <img id="share-btn" src="./images/icon-dm.png" alt="share button">
+                </div>
+
+                <div class="likes">
+                    <p>${likes} likes</p>
+                </div>
+
+                <div class="post-comment">
+                    <p class="username">${username}</p>
+                    <p class="comment">${comment}</p>
+                </div>
+            </div>
+        </article>
+    `
+}).join('')
+
+postsSection.innerHTML = postsHtml
